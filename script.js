27635916419bb7d3957619b636faf4f6e411ef7e
franzon <brfranzon@gmail.com>
2020-06-08 02:12:26 +0200

    commit

--- script.js
+++ script.js
@@ -1,72 +1,17 @@
 
-var vel, dx, dy, px, py, jogador;
-var anime = setInterval(game, 1000);
-
-//window.addEventListener("load", inicia);
-
-$(window).one('scroll', () => {
-    // myFunction();
-});
-
-
-
-function myFunction() {
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        document.getElementById("jogador").style.backgroundColor = "gray";
-        document.getElementById("jogador").style.opacity = 0.2;
-        inicia();
-
-
-        //  } else {
-        //    document.getElementById("myP").innerHTML = "aaaaaaaaaaa";
-        //  }
-    }
+// Grafiken vorladen
+Grafik = new Array(15);
+for (i = 0; i < 15; i++) {
+    Grafik[i] = new Image();
+    Grafik[i].src = "Globen/" + i + ".gif";
 }
 
 
-function inicia() {
-    vel = 200;
-    dx = 1;
-    dy = 0;
-    px = 0;
-    py = 0;
-    jogador = document.getElementById("jogador");
-    anime;
+
+i = 14;
+function rotation() {
+    document.getElementsByTagName('img')["globus"].src = "Globen/" + i + ".gif";
+    if (i > 0) i--; else i = 14;
+
+    setTimeout('rotation()', 100);
 }
-
-
-function game() {
-    // s = s0 + vt
-    px = px + vel * dx;
-    // py = py + vel*dy;
-
-    jogador.style.left = px + "px";
-    // jogador.style.bottom = px+"px";
-
-    if (px >= window.innerWidth) {
-        dx = -1;
-    } else if (px <= 0) {
-        dx = 1;
-        // clearInterval(anime);
-    }
-
-}
-
-
-/** mouseover */
-pic.addEventListener("mouseover", ()=>{
-    let name = document.getElementById("name");
-    name.style.color = "yellow";
-    name.style.transitionDuration = "2s";
-
-});
-
-pic.addEventListener("mouseout", ()=>{
-    let name = document.getElementById("name");
-    name.style.color = "black";
-    name.style.transitionDuration = "2s";
-
-});
-
-   
-
